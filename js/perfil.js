d1ffa442c0aced25e11ab8f1b17af21057e180ae
ulisesVela16 <98621025+ulisesVela16@users.noreply.github.com>
2023-10-20 18:12:23 -0600

    archivo

--- js/perfil.js
+++ js/perfil.js
@@ -8,3 +8,14 @@ const fileInput = document.getElementById("file-input");
                 profileImage.src = objectURL;
             }
         });
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const descargarBtn = document.getElementById("descargarBtn");
+    
+            descargarBtn.addEventListener("click", function (e) {
+                const confirmacion = confirm("¿Estás seguro de que deseas descargar tu currículum?");
+                if (!confirmacion) {
+                    e.preventDefault(); // Evita que el enlace se abra si el usuario no confirma
+                }
+            });
+        });
